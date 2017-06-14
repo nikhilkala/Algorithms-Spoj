6784a633944ad876a953c3986a12b0d5a72645bc
nikhil kala <nikhilkala8@gmail.com>
2017-06-14 12:52:10 +0530

    Create ACPC10A.cpp

---
+++ ACPC10A.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ int a1,a2,a3;
+ while(1)
+ {
+	cin>>a1>>a2>>a3;
+ 	if(a1==0)
+ 	if(a2==0)
+ 	if(a3==0)
+ 	break;
+ 	int r1,r2,t;
+ 	r1=a2-a1;
+ 	r2=a3-a2;
+ 	if(r1==r2)
+ 	{
+ 		t=a3+r1;
+ 		cout<<"AP "<<t<<endl;
+ 	}
+ 	else{
+ 		r1=a3/a2;
+ 		t=a3*r1;
+		cout<<"GP "<<t<<endl; 	
+ 	}
+ 	
+ }
+return 0;
+}
