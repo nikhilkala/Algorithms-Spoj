7cdc0b5b45ba3353ba09dcfd5a71ab510a125594
oo7rm <rf.rishabh@gmail.com>
2017-10-30 01:09:06 +0530

    Update ABSYS.cpp

--- ABSYS.cpp
+++ ABSYS.cpp
@@ -8,7 +8,7 @@ int main() {
 	{
 		char str1[100001],str2[100001],str3[100001],ch1,ch;
 		int i,j,num1=0,num2=0,num3=0,flag=0;
-	scanf("%s %c %s %c %s",&str1,&ch,&str2,&ch1,&str3);		
+		scanf("%s %c %s %c %s",&str1,&ch,&str2,&ch1,&str3);		
 		for(i=0;str1[i]!='\0';i++)
 			{
 				if(str1[i]=='m')
@@ -18,7 +18,7 @@ int main() {
 					for(j=0;str3[j]!='\0';j++)
 						num3 = num3*10 + str3[j] - 48;
 					num1 = num3 - num2;
-				flag=1;
+					flag=1;
 					break;
 				}
 			}
@@ -26,12 +26,13 @@ int main() {
 			{
 				if(str2[i]=='m')
 				{
-					for(j=0;str1[j]!='\0';j++)
+					for(j=0;str1[j]!='\0';j++){
 						num1 = num1*10 + str1[j] - 48;
+					}
 					for(j=0;str3[j]!='\0';j++)
 						num3 = num3*10 + str3[j] - 48;
-					num2 = num3 - num1;
-					break;
+						num2 = num3 - num1;
+						break;
 				}
 			}
 			for(i=0;str3[i]!='\0' && flag==0;i++)
